47095dd5779bab6ca26f8d993700f5410c2fcdd0
dany <danylobyshkin@gmail.com>
2023-11-07 13:15:39 -0500

    browser automatically opens when closed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.chrome_filepath = '/usr/bin/google-chrome-stable';
-config.chrome_launch_flags = '--remote-debugging-port=9222  --no-first-run  --no-default-browser-check 2> browser.log & ';
+config.chrome_launch_flags = '--remote-debugging-port=9222  --no-first-run  --no-default-browser-check 2> browser.log &';
 config.chrome_use_headless = false
 config.to_reload_pages = true
 config.page_reload_timeout = 5000 // in ms
