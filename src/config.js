f9cd7e576285f291cb58763912e4c2897252e53c
dany <danylobyshkin@gmail.com>
2023-11-18 18:20:35 -0500

    fixing chrome being detectable

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 var config = {};
 
 config.chrome_filepath = '/usr/bin/google-chrome-stable';
-config.chrome_launch_flags = '--remote-debugging-port=9222 --disable-ipc-flooding-protection --no-sandbox   --no-default-browser-check 2> browser.log &';
-config.chrome_use_headless = false
+config.chrome_launch_flags = [
+    '--remote-debugging-port=9222',
+    '--disable-ipc-flooding-protection',
+    '--no-default-browser-check'
+];
+config.chrome_use_headless = true
 config.to_reload_pages = true
 config.page_reload_timeout = 5000 // in ms
 
--- src/server/start-persistent-browser.js
+++ src/server/start-persistent-browser.js
@@ -3,58 +3,47 @@ const stealth = require('puppeteer-extra-plugin-stealth')();
 chromium.use(stealth);
 const {sleep, isRunningInDocker} = require('../helpers/helpers.js')
 var config = require('../config.js');
+const net = require('net');
 var runHeadless = config.chrome_use_headless
 var launch_flags = config.chrome_launch_flags
 
 async function isPortInUse(port) {
-  return new Promise((resolve) => {
-    const server = net.createServer();
+	return new Promise((resolve, reject) => {
+		const server = net.createServer();
 
-    server.once('error', (err) => {
-      if (err.code === 'EADDRINUSE') {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
+		server.once('error', (err) => {
+			if (err.code === 'EADDRINUSE') {
+				resolve(true);
+			} else {
+				reject(err); // Handle unexpected errors
+			}
+		});
 
-    server.once('listening', () => {
-      server.close();
-      resolve(false);
-    });
+		server.once('listening', () => {
+			server.close();
+			resolve(false);
+		});
 
-    server.listen(port, '127.0.0.1');
-  });
-}
-
-function execute(command){
-	const chromeProcess = spawn(command, { detached: true, stdio: ['ignore','ignore','ignore']});
-	chromeProcess.unref();
+		server.listen(port, '0.0.0.0');
+		server.unref(); 
+	});
 }
 
 async function runBrowser(){
 	if(runHeadless || isRunningInDocker()){
 		console.log("Running headless browser")
-		runHeadless = true;
 	}
 	else{
 		console.log("Running headed browser")
 	}
 
-	const context = await chromium.launchPersistentContext( userDataDir="./chrome-data", {
-        executablePath: '/usr/bin/google-chrome-stable',
-        handleSIGINT: false,
-        headless: runHeadless,
-        args:launch_flags
-    })
-
-
-
-	// const command = 'nohup ' + config.chrome_filepath + ' ' + launch_flags;
-	// console.log("Opening browser")
-	// execute(command)
-	// waiting for the browser to open
-	await sleep(1000)
+	const context = await chromium.launchPersistentContext( userDataDir="../chrome-context-data", {
+		executablePath: '/usr/bin/google-chrome-stable',
+		handleSIGINT: false,
+		args:launch_flags,
+	})
+    const page = context.pages()[0];
+    await page.goto('https://bot.sannysoft.com')
 }
 
 async function runPersistentBrowser(){
@@ -64,8 +53,7 @@ async function runPersistentBrowser(){
 }
 
 (async () => {
-	// setInterval(async () => {
-	// 	await runPersistentBrowser()
-	// }, 1000);
-	await runBrowser()
+	setInterval(async () => {
+		await runPersistentBrowser()
+	}, 1000);
 })();
